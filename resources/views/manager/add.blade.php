b3b8b753c44983e2c066531b711eda18558a2e7b
codedsakura <sakuracoded@gmail.com>
2021-06-19 14:22:11 +0300

    add manager list and add

---
+++ resources/views/manager/add.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Add Manager') }}
+        </h2>
+    </x-slot>
+    <form method="POST" action="{{ route('managers.store') }}" enctype="multipart/form-data">
+        @csrf
+
+        <x-input inputFor="employee" required>{{ __('Personal ID') }}</x-input>
+        <x-input inputFor="password" type="password" required>{{ __('Password') }}</x-input>
+
+        <x-submit-btn />
+    </form>
+</x-app-layout>
