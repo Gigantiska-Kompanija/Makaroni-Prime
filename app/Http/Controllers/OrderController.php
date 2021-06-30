779579def2f74f0cf8675ed3e980541e65ed6d1d
codedsakura <sakuracoded@gmail.com>
2021-06-30 15:27:39 +0300

    fix client not being able to see own order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class OrderController extends Controller {
 
     public function show($id) {
         $order = Order::findOrFail($id);
+        if (!Auth::guard('manager')->check()) {
+            if ($order->clientID != Auth::user()->id) {
+                throw new NotFoundHttpException();
+            }
+        }
         return view('orders.info', compact('order'));
     }
 }
