da726f60b97d306dfa02404716c76b53c9530485
Artūrs <A@DESKTOP-ALDTUIJ>
2021-06-30 13:34:20 +0300

    edited employee info view

--- resources/views/employees/info.blade.php
+++ resources/views/employees/info.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
     <div class="d-flex justify-content-between align-items-center">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Employee') }} {{ $employee->personalId }}
+        {{ __('Employee') }} {{ $employee->firstName }} {{ $employee->lastName }}
         </h2>
         <a class="btn btn-warning" href={{ route("employees.edit", $employee->personalId) }}><i class="fas fa-pen"></i></a>
     </div>
